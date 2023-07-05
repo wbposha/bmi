e05b82fd4d58dad4638e899bd33b09c02d041a73
wbposha <135736179+wbposha@users.noreply.github.com>
2023-07-06 00:57:07 +0400

    Update BmiService.java

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,3 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        BmiService service = new BmiService();
+        int high = (int) 1.87;
+        int weight = 98;
+        int index = high / weight;
+        System.out.println((int)index);
+    }
+}
+
+
+
+
+### Class
+
 public class BmiService {
     public int calculate(int weight, int high) {
         int result;
@@ -11,4 +26,10 @@ public class BmiService {
     }
 
 }
+
+
+
+
+
+
 
